c98cb90763523c5ee47f4a88e79691fcc117ecfa
Stefan Lier <lier.stefan@gmail.com>
2024-07-14 12:23:48 +0200

    Link git-related config instead of setting it directly

--- configure.sh
+++ configure.sh
@@ -1,25 +1,25 @@
 #/!bin/zsh
 set -e
 
+function link_file {
+    echo "Linking $1"
+
+    [ -f $1 ] && cp $1 $1.bak
+    ln -fs $(pwd)/$1 $1
+}
+
+# Initialise config-dir if not already present
+mkdir -p ~/.config 
+
+# Setting default shell
 echo "Set the default shell to zsh managed with brew"
 sudo /bin/zsh -c 'echo "$(brew --prefix zsh)/bin/zsh" >> /etc/shells'
 chsh -s $(brew --prefix zsh)/bin/zsh $(whoami)
 
-echo "Set git config"
-git config --global user.name "Stefan Lier"
-git config --global user.email "lier.stefan@gmail.com"
-
-echo "Initialise Starship Config"
-mkdir -p ~/.config && ln -fs $(pwd)/starship.toml ~/.config/starship.toml
-
-echo "Linking .zshrc"
-cp ~/.zshrc ~/.zshrc.bak
-ln -fs $(pwd)/.zshrc ~/.zshrc
-
-echo "Linking .zprofile"
-cp ~/.zprofile ~/.zprofile.bak
-ln -fs $(pwd)/.zprofile ~/.zprofile
-
-echo "Linking .vimrc"
-cp ~/.vimrc ~/.vimrc.bak
-ln -fs $(pwd)/.vimrc ~/.vimrc
+# Linking config files
+link_file ~/.gitconfig
+link_file ~/.gitignore.global
+link_file ~/.config/starship.toml
+link_file ~/.zshrc
+link_file ~/.zprofile
+link_file ~/.vimrc
